7052c00a7591c58b9e963b4e4a6b2bd8bd7d5a57
shivasv97 <shivasv97@gmail.com>
2017-04-19 16:08:12 +0530

    Add files via upload

---
+++ mongo2.js
@@ -0,0 +1,9 @@
+var db
+
+MongoClient.connect('your-mongodb-url', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
